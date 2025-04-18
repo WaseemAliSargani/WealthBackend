0144e3e85004444df41208f729d18e7d9e77167a
Waseem Ali Sargani <waseemalisargani88@gmail.com>
2025-04-18 22:37:27 +0500

    Add Vercel serverless configuration

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const connectDB = async () => {
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
-    process.exit(1);
+    throw new Error("MongoDB Connection Failed");
   }
 };
 
